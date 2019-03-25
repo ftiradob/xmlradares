ee4d746b3be7a75823135598c22bb3b020fffd48
Fernando Tirado <fernando.tb.95@gmail.com>
2019-03-25 21:35:23 +0100

    Añado ejercicios 3 y 4

--- radarestirado.py
+++ radarestirado.py
@@ -11,6 +11,27 @@ def cuentaradares(doc):
     print("Contamos con",len(cuenta),"radares en el total de todas las carreteras.")
     print("")
 
+def mostrarcarre(doc):
+    provincia=input("Introduzca una provincia: ")
+    listacarre=doc.xpath("/RAIZ/PROVINCIA[NOMBRE='%s']/CARRETERA/DENOMINACION/text()"%(provincia))
+    print("")
+    print("Contamos con", len(listacarre),"radares en %s." %(provincia))
+    print("Los nombres de sus carreteras son: ")
+    print("")
+    return list(set(listacarre))
+
+def mostrarprovi(doc,carretera):
+    listaprovi=doc.xpath("//CARRETERA[DENOMINACION='%s']/../NOMBRE/text()"%(carretera))
+    print("")
+    print("La carretera %s pasa por: " %(carretera))
+    print("")
+    return listaprovi
+
+def contarad(doc,carretera):
+    cuentarad=doc.xpath("//CARRETERA[DENOMINACION='%s']/RADAR/SENTIDO/text()"%(carretera))
+    print("Cuenta con",len(cuentarad),"radares.")
+    print("")
+
 print("")
 while True:
     print("1. Mostrar el nombre de las provincias de las que tenemos información sobre radares.")
@@ -28,6 +49,16 @@ while True:
         print("")
     elif opcion==2:
         cuentaradares(doc)
+    elif opcion==3:
+        for i in mostrarcarre(doc):
+            print("-",i)
+        print("")
+    elif opcion==4:
+        carretera=input("Introduzca una carretera: ")
+        for i in mostrarprovi(doc,carretera):
+            print("-",i)
+        print("")
+        contarad(doc,carretera)
     elif opcion==0:
         print("Adios")
         break
